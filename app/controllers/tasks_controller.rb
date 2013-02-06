f94f9d055a1d53d81ca12f4a871fd01044ade7e2
ya.jeks <ya.jeks@yandex.ru>
2013-02-06 13:57:16 +0400

    Task fixes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -6,15 +6,22 @@ class TasksController < ApplicationController
   end
 
   def start
-    @task = Task.find params[:task]
-    render json: @task.errors, status: :unprocessable_entity unless @task.start
-    head :no_content
+    @task = Task.find params[:id]
+    if @task.start
+      head :no_content
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
   end
 
   def complete
-    @task = Task.find params[:task]
-    render json: @task.errors, status: :unprocessable_entity unless @task.complete
-    head :no_content
+    @task = Task.find params[:id]
+    if @task.update_attributes(result: params[:result])
+      @task.complete!
+      head :no_content
+    else
+      render json: @task.errors, status: :unprocessable_entity
+    end
   end
 
 end
