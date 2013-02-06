d3a5a50c142d1ee0d59c42702b24854482ad4969
ya.jeks <ya.jeks@yandex.ru>
2013-02-06 15:13:07 +0400

    move auth to tasks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  before_filter :authenticate_client
-
-  private
-    def authenticate_client
-      @client = Client.find_by_keyword( params[:keyword] )
-      render :status => :forbidden, :text => "Forbidden" unless @client
-    end
 end
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,4 +1,5 @@
 class TasksController < ApplicationController
+  before_filter :authenticate_client
 
   def index
     @tasks = @client.tasks.created
@@ -23,5 +24,11 @@ class TasksController < ApplicationController
       render json: @task.errors, status: :unprocessable_entity
     end
   end
+
+  private
+    def authenticate_client
+      @client = Client.find_by_keyword( params[:keyword] )
+      render :status => :forbidden, :text => "Forbidden" unless @client
+    end
 
 end
